80acb1ea0d20ce96b9e69f000680cb33345f97df
Anton Plauche <antonplauche92@gmail.com>
2020-10-07 18:51:52 -0500

    added json-server database for posts

--- src/pages/BlogList.js
+++ src/pages/BlogList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PostItem from "../components/PostItem";
 import Header from "../components/Header";
 import Container from "../components/Container";
@@ -9,17 +9,40 @@ import SidebarLayout from "./SidebarLayout";
 import styled from "@emotion/styled";
 
 function BlogList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const res = await fetch("/posts");
+        const postsData = await res.json();
+
+        setPosts(posts.concat(postsData));
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <SidebarLayout>
       <Sidebar />
-      <div class="main">
+      <div className="main">
         <Header />
         <Container width="800px" padding="50px 20px">
           <SearchBar />
-          <PostItem title="blog post 1" date="10-03-2020" published={true} />
-          <PostItem title="blog post 2" date="10-06-2020" published={false} />
-          <PostItem title="blog post 3" date="10-03-2020" published={true} />
-          <PostItem title="blog post 4" date="10-06-2020" published={false} />
+          {posts.map((item) => {
+            return (
+              <PostItem
+                key={item.id}
+                title={item.title}
+                date={item.date}
+                published={item.published}
+              />
+            );
+          })}
         </Container>
       </div>
     </SidebarLayout>
