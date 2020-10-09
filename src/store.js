a9036168647238bcb78783ff57853fa8bc28a568
Anton Plauche <antonplauche92@gmail.com>
2020-10-08 22:32:07 -0500

    installed immer and set up useImmerReducer in store.js

--- src/store.js
+++ src/store.js
@@ -1,18 +1,25 @@
 import React, { useState, useContext, useReducer, createContext } from "react";
+import { useImmerReducer } from "use-immer";
 
 export const Context = createContext();
 
 function GlobalContextProvider(props) {
-  const initialState = {};
+  const initialState = {
+    loggedIn: false,
+  };
 
-  const reducer = (state, action) => {
+  const reducer = (draft, action) => {
     switch (action.type) {
       case "login":
-        return { ...state, ...action.payload };
+        draft.loggedIn = true;
+        break;
+      case "logout":
+        draft.loggedIn = false;
+        break;
     }
   };
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useImmerReducer(reducer, initialState);
 
   const initialContext = { state, dispatch };
 
